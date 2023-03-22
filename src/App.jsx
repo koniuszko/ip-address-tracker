48e9bdfd256cd0cd3e1fc448f40f2e3dbba63b12
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2023-03-22 20:52:20 +0100

    map integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
-import arrowIcon from '../public/images/icon-arrow.svg'
+import arrowIcon from './images/icon-arrow.svg'
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+
 
 function App() {
-
-
+    // const map = L.map('map', {
+    //     center: [51.505, -0.09],
+    //     zoom: 13
+    // });
     return (
         <div className="App">
             <header className="banner">
@@ -33,7 +37,19 @@ function App() {
                     </div>
                 </div>
             </header>
-            <main className="map"></main>
+            <div id="map">
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br/> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
         </div>
     )
 }
